f932bfffd261a762f1628d50bcd328a880c365b7
Vasyl Poremchuk <nleveryone@gmail.com>
2023-02-15 00:13:34 +0200

    Minor changes

--- square_matrix_converter/__init__.py
+++ square_matrix_converter/__init__.py
@@ -1,3 +1,3 @@
-from convert_matrix import convert_matrix
-from convert_square_matrix import convert_square_matrix
-from parse_matrix import parse_matrix
+from square_matrix_converter.convert_matrix import convert_matrix
+from square_matrix_converter.convert_square_matrix import convert_square_matrix
+from square_matrix_converter.parse_matrix import parse_matrix
